5e6afb5a99a29394939c256c0b043a1ac9779565
0x424D <30124318+0x424D@users.noreply.github.com>
2017-11-05 17:45:36 +0000

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,16 @@
+# Write a recursive function called isPalindrome.  
+# The function should take one input, S, which is assumed a string.  
+# It should produce a Boolean output depending 
+# - True if the string is a palindrome 
+# - False if not.
+
+def isPalindrome(S):
+  if len(S) == 0:
+    return True
+  
+  if S[0] != S[-1]:
+    return False
+  
+  return isPalindrome(S[1:len(S) - 1])
+  
+print(isPalindrome("a"))
